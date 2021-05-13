f15c0460784e57e1acac1490d96a15110f815fb5
Akiyama <nhatvu148@gmail.com>
2021-05-13 18:20:59 +0900

    initial commit

---
+++ ToPdf/Program.cs
@@ -0,0 +1,32 @@
+﻿using OfficeConverter;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace ToPdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathDirectory = $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}";
+            string docPath = $"{pathDirectory}/../対象船のデータ取得状況.docx";
+
+            if (args == null || args.Length == 0)
+            {
+                // no arguments
+            }
+            else
+            {
+                docPath = Convert.ToString(args[0]);
+            }
+
+            Console.WriteLine($"Start converting {docPath} to pdf...");
+            using (var converter = new Converter())
+            {
+                converter.Convert(docPath, docPath.Replace(".docx", ".pdf"));
+            }
+            Console.WriteLine("Finished!");
+        }
+    }
+}
